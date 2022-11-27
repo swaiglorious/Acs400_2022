698fcb8ac2e9bcebd1debb0251b266fed3a83885
Glorious <swaiglorious@gmail.com>
2022-11-27 14:17:30 +0300

    Added missing /app folder

---
+++ app/src/main/java/com/example/taxpayer/DisplayDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.taxpayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_data)
+    }
+}
